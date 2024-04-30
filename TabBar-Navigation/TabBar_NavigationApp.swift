3166344a5719a78fdbe9f1a94a88e0ad157a22d3
Daniel Budusan <danibudusan@gmail.com>
2024-04-30 22:57:58 +0300

    first commit

---
+++ TabBar-Navigation/TabBar_NavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  TabBar_NavigationApp.swift
+//  TabBar-Navigation
+//
+//  Created by Daniel Budusan on 30.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct TabBar_NavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
