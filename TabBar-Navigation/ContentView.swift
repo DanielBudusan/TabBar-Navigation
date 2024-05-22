f76b47cad6f293e2953f19b55850c8cd5f3b4793
Daniel Budusan <danibudusan@gmail.com>
2024-05-22 16:37:28 +0300

    test

--- TabBar-Navigation/ContentView.swift
+++ TabBar-Navigation/ContentView.swift
@@ -20,7 +20,7 @@ import SwiftUI
 //    case unFavorite = "star.slash"
 //    case favorite = "star.fill"
 //}
-
+// test
 struct ContentView: View {
     @State private var previousSelectedTab: TabBarItem = .init(iconName: "house", title: "home")
     @State private var selectedTab: TabBarItem = .init(iconName: "house", title: "home")
